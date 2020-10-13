5af588c143c76b33a4e16754becf45c4b92739af
GitLD <30947306+GitLD@users.noreply.github.com>
2020-10-13 14:44:47 +0800

    commit-frozenlake(q-learning)

---
+++ Chapter_2[Q-learning]/frozen_lake.py
@@ -0,0 +1,108 @@
+import numpy as np 
+import gym
+import random
+from matplotlib import pyplot as plt
+
+# Hyperparameter
+total_episodes = 20000
+learning_rate = 0.8
+max_steps = 99
+gamma = 0.95
+
+epsilon = 1.0
+max_epsilon = 1.0
+min_epsilon = 0.01
+decay_rate = 0.005
+
+test_episodes = 15
+train = False
+test = True
+
+# Make Env
+env = gym.make('FrozenLake-v0')
+
+# Initialize Q-Table
+action_size = env.action_space.n
+state_size = env.observation_space.n
+qtable = np.zeros((state_size, action_size))
+
+# Train
+if train:
+    rewards = []
+
+    for episode in range(total_episodes):
+        state = env.reset()
+        step = 0
+        done = False
+        total_rewards = 0
+
+        for step in range(max_steps):
+            tradeoff = random.uniform(0, 1)
+            if tradeoff > epsilon:
+                # Exploitation
+                action = np.argmax(qtable[state, :])
+            else:
+                # Exploration
+                action = env.action_space.sample()
+        
+            new_state, reward, done, info = env.step(action)
+
+            # Update Q Table
+            dq = reward + gamma * np.max(qtable[new_state, :]) - qtable[state, action]
+            qtable[state, action] = qtable[state, action] + learning_rate * dq
+            
+            # Move to next state
+            total_rewards += reward
+            state = new_state
+
+            if done:
+                break
+
+        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+        rewards.append(total_rewards)
+    np.save('Frozen-Lake-Qtable.npy', qtable)
+    print('Score over time: ' + str(sum(rewards)/total_episodes))
+    print('Q-Table'.center(40,'*'))
+    print(qtable)
+    plt.plot(range(1,total_episodes+1), np.cumsum(rewards)/range(1,total_episodes+1))
+    plt.title("Training Process")
+    plt.xlabel('Train Episode')
+    plt.ylabel('Average Total Rewards')
+    plt.xlim(0, total_episodes)
+    plt.ylim(bottom=0)
+    plt.show()
+
+# Test
+if test:
+    rewards = []
+    qtable = np.load('Frozen-Lake-Qtable.npy')
+    for episode in range(test_episodes):
+        state = env.reset()
+        step = 0
+        done = False
+        total_rewards = 0
+        print(('EPSIODE = %d'%(episode+1)).center(40,'*'))
+
+        for step in range(max_steps):
+            action = np.argmax(qtable[state, :])
+            new_state, reward, done, info = env.step(action)
+
+            total_rewards += reward
+            state = new_state
+
+            if done:
+                env.render()
+                print('Number of steps %d'%step)
+                break
+
+        rewards.append(total_rewards)
+    plt.bar(range(1, test_episodes+1), rewards)
+    plt.title("Test Process[Successful Rate:%.2f%%]"%(sum(rewards)/len(rewards)*100))
+    plt.xlabel('Test Episode')
+    plt.ylabel('Total Rewards')
+    # plt.xlim(0, test_episodes+1)
+    plt.ylim(bottom=0)
+    plt.show()
+
+        
+    env.close()
